0798bbb28a0713aa74460c2fd102da71c9341c40
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-09-14 20:58:14 +0900

    More strict test for sampling points of three_point_ph

--- test/python/three_point_ph_test.py
+++ test/python/three_point_ph_test.py
@@ -47,7 +47,7 @@ class TestMethods(unittest.TestCase):
         Lambda = 10.0
         beta = 0.2
         wmax = Lambda/beta
-        phb = ThreePointPHBasis(boson_freq, Lambda, beta)
+        phb = ThreePointPHBasis(boson_freq, Lambda, beta, 1e-12)
         Nl = phb.Nl
 
         iwn_f = lambda n : 1J*(2*n+1)*numpy.pi/beta 
@@ -67,13 +67,13 @@ class TestMethods(unittest.TestCase):
                 coeffs = _compute_Gl(phb, pole, s1, s2, r)
                 Giwn_ref = _compute_Giw(phb, pole, s1, s2, r, n1, n2, boson_freq)
                 Giwn = numpy.sum(prj * coeffs)
-                assert numpy.abs(Giwn_ref/Giwn - 1) < 1e-8
+                assert numpy.abs(Giwn_ref/Giwn - 1) < 1e-7
 
     def test_sampling_points_matsubara(self):
         boson_freq = 10
         Lambda = 10.0
         beta = 0.2
-        alpha = 1e-10
+        alpha = 1e-15
         augmented = False
 
         wmax = Lambda/beta
@@ -87,47 +87,48 @@ class TestMethods(unittest.TestCase):
 
         #n1n2_check = [(20, 20)]
         n1n2_check = []
-        niw = 10
-        for i,j in product(range(-niw,niw), repeat=2):
+        niw = 1000
+        for i,j in product(range(-niw,niw,10), repeat=2):
             n1n2_check.append((i,j))
-        prj_check = phb.projector_to_matsubara_vec(n1n2_check)
         S = phb.normalized_S()
-
-        #for n1n2 in sp:
-            #print("sp ", n1n2[0], n1n2[1])
+        prj_check = numpy.array(phb.projector_to_matsubara_vec(n1n2_check))[:, :,:,:,:,:] * S[None, :]
 
         # r = 0: Fermion, Fermion
         # r = 1: Boson, Fermion
         # r = 2: Boson, Fermion
+        #for alpha, r in product([1e-25, 1e-20, 1e-16, 1e-14, 1e-12, 1e-10], range(1)):
+        #for alpha, r in product([1e-40], range(1)):
         for r in range(3):
             for s1, s2 in product(range(2), repeat=2):
                 coeffs_ref = _compute_Gl(phb, pole, s1, s2, r)
                 Giwn = numpy.array([_compute_Giw(phb, pole, s1, s2, r, n1n2[0], n1n2[1], boson_freq) for n1n2 in sp])
 
-                S[:,:,:,:,:] = 1
-                prj_rho = numpy.array(phb.projector_to_matsubara_vec(sp)) * S[None, :,:,:,:,:]
+                prj = numpy.array(phb.projector_to_matsubara_vec(sp))[:, :,:,:,:,:] * S[None, :]
 
-                prj_rho_mat = prj_rho[:,r,s1,s2,:,:].reshape((n_sp, Nl*Nl))
-                coeffs_rho = ridge_complex(prj_rho_mat, Giwn, alpha).reshape((Nl,Nl))
-                coeffs = coeffs_rho * S[r,s1,s2,:,:]
+                prj_mat = prj[:,:,:,:,:,:].reshape((n_sp, 3*2*2*Nl*Nl))
+                coeffs = ridge_complex(prj_mat, Giwn, alpha).reshape((3,2,2,Nl,Nl)) * S
 
-                #Giwn_check_ref = numpy.array([_compute_Giw(phb, pole, s1, s2, r, n1n2[0], n1n2[1], boson_freq) for n1n2 in n1n2_check])
-                #Giwn_check = numpy.array([numpy.sum(p * coeffs) for p in prj_check])
-                #print(Giwn_check_ref, Giwn_check)
-                #idx = 0
-                #for i in range(-niw,niw):
-                    #for j in range(-niw,niw):
-                        #print(i, j, Giwn_check_ref[idx].real, Giwn_check_ref[idx].imag, Giwn_check[idx].real, Giwn_check[idx].imag)
-                        #idx += 1
+                #Giwn_reconst = numpy.dot(prj_mat, (coeffs/S).reshape((3*2*2*Nl*Nl)))
+                Giwn_check_ref = numpy.array([_compute_Giw(phb, pole, s1, s2, r, n1n2[0], n1n2[1], boson_freq) for n1n2 in n1n2_check])
+                Giwn_check = numpy.dot(prj_check.reshape((len(n1n2_check), 3*2*2*Nl*Nl)), (coeffs/S).reshape((3*2*2*Nl*Nl)))
+                #print("debbu ", Giwn_check_ref.shape)
+                #print("diff ", alpha, numpy.amax(numpy.abs(Giwn_check - Giwn_check_ref)))
+                print("diff ", alpha, numpy.amax(numpy.abs(Giwn_check - Giwn_check_ref)))
+                self.assertLessEqual(numpy.amax(numpy.abs(Giwn_check - Giwn_check_ref)), 1e-5)
+                #print(Giwn_reconst[:10])
+                #print(Giwn[:10])
+
+                #print("# ", r, s1, s2)
+                #for i in range(Nl):
+                    #for j in range(Nl):
+                        #print(i, j, numpy.abs(coeffs[r,s1,s2,i,j]), numpy.abs(coeffs_ref[r,s1,s2,i,j]))
                     #print("")
-
-                #for r,s1,s2 in product(range(3), range(2), range(2)):
-                print("# ", r, s1, s2)
-                for i in range(Nl):
-                    for j in range(Nl):
-                        print(i, j, numpy.abs(coeffs[i,j]), numpy.abs(coeffs_ref[r,s1,s2,i,j]))
-                    print("")
                 #print(r, s1, s2, numpy.amax(numpy.abs(coeffs-coeffs_ref[r,s1,s2,:,:])))
+                #idx = 0
+                #for i,j in product(range(-niw,niw,10), repeat=2):
+                    #if i == j:
+                        #print(i, j, Giwn_check[idx].real, Giwn_check[idx].imag, Giwn_check_ref[idx].real, Giwn_check_ref[idx].imag)
+                    #idx += 1
 
 if __name__ == '__main__':
     unittest.main()
