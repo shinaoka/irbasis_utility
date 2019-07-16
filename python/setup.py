ad1dd808c1a12d64d7b66e4783f0c7c94a01fe0b
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2019-07-16 14:14:46 +0900

    Version bump to 0.6

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='irbasis utility',
 
-    version='0.5',
+    version='0.6',
 
     description='Utility python libraries for irbasis',
 
