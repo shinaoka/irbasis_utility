0eabebcae097fdef3c6899b2ee6f1e967b4f87e3
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2019-08-20 13:23:03 +0900

    Bump to version 0.8. __version__ is now available

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='irbasis utility',
 
-    version='0.7',
+    version='0.8',
 
     description='Utility python libraries for irbasis',
 
