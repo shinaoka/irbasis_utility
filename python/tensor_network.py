0692cefc3e59fae0283e6d93adec00f826c26f3c
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2019-11-25 10:36:32 +0900

    Implemented tensor nework representation of conj(a) * b

--- python/tensor_network.py
+++ python/tensor_network.py
@@ -31,11 +31,20 @@ class Tensor(object):
         self._shape = shape
         self._is_conj = is_conj
 
+    def __str__(self):
+        if self._is_conj:
+            return "conj({}){}".format(self._name, self._shape)
+        else:
+            return "{}{}".format(self._name, self._shape)
+
+    def __repr__(self):
+        return self.__str__()
+
     def conjugate(self):
         """
         Create a complex conjugate
         """
-        return Tensor(self._name, self._shape, not self._conj)
+        return Tensor(self._name, self._shape, not self._is_conj)
 
     @property
     def name(self):
@@ -85,15 +94,16 @@ class TensorNetwork(object):
         if numpy.amin(unique_subscripts) < 0:
             raise RuntimeError("Subscripts must be 0 or positive.")
 
-        # Mark outer subscript
-        #  An outer subscript appears only once.
+        # Mark external subscript
+        #  An external subscript appears only once.
         counter_subscripts = numpy.zeros(num_subscripts, dtype=int)
         for s in sum(subscripts, ()):
             counter_subscripts[s] += 1
-        self._is_outer_subscript = numpy.array(counter_subscripts == 1)
-        self._outer_subscripts = unique_subscripts[self._is_outer_subscript]
+        self._is_external_subscript = numpy.array(counter_subscripts == 1)
+        self._external_subscripts = unique_subscripts[self._is_external_subscript]
 
         self._tensors = tensors
+        self._unique_subscripts = unique_subscripts
         self._num_tensors = len(tensors)
         self._subscripts = subscripts
         self._contraction_path = None
@@ -101,7 +111,7 @@ class TensorNetwork(object):
         # Create str ver. of subscripts
         ints_to_alphabets = lambda integers: ''.join(map(_to_alphabet, integers))
         left_str = ','.join(map(ints_to_alphabets, self._subscripts))
-        right_str = ints_to_alphabets(self._outer_subscripts)
+        right_str = ints_to_alphabets(self._external_subscripts)
         self._str_sub = left_str + '->' + right_str
 
     @property
@@ -117,8 +127,12 @@ class TensorNetwork(object):
         return self._subscripts
 
     @property
-    def outer_subscripts(self):
-        return self._outer_subscripts
+    def external_subscripts(self):
+        return self._external_subscripts
+
+    @property
+    def unique_subscripts(self):
+        return self._unique_subscripts
 
     def find_contraction_path(self, verbose=False):
         """
@@ -156,3 +170,73 @@ class TensorNetwork(object):
                 arrays.append(values_of_tensors[t.name])
 
         return numpy.einsum(self._str_sub, *arrays, optimize=self._contraction_path)
+
+def conj_a_b(a, b):
+    """
+    Contract a tensor network representation of <a|b>
+    a and b must share the same subscripts for the external indices.
+    In the resulting tensor network, the contracted indices have the smallest subscripts.
+
+    :param a: TensorNetwork
+    :param b: TensorNetwork
+    """
+
+    if not numpy.all(a.external_subscripts == b.external_subscripts):
+        raise RuntimeError("Outer indices do not match!")
+
+    num_ext_idx = len(a.external_subscripts)
+    num_idx_a = len(a.unique_subscripts)
+    num_idx_b = len(b.unique_subscripts)
+
+    # Create map from old subscripts to new subscripts
+    new_subscripts_a = numpy.empty(num_idx_a, dtype=int)
+    new_subscripts_b = numpy.empty(num_idx_b, dtype=int)
+
+    # External indices
+    next_new_subscript = 0
+    for i, subscript in enumerate(a.unique_subscripts):
+        if subscript in a.external_subscripts:
+            new_subscripts_a[i] = next_new_subscript
+            next_new_subscript += 1
+
+    next_new_subscript = 0
+    for i, subscript in enumerate(b.unique_subscripts):
+        if subscript in b.external_subscripts:
+            new_subscripts_b[i] = next_new_subscript
+            next_new_subscript += 1
+
+    # Internal indices in a
+    for i, subscript in enumerate(a.unique_subscripts):
+        if not subscript in a.external_subscripts:
+            new_subscripts_a[i] = next_new_subscript
+            next_new_subscript += 1
+
+    # Internal indices in b
+    for i, subscript in enumerate(b.unique_subscripts):
+        if not subscript in b.external_subscripts:
+            new_subscripts_b[i] = next_new_subscript
+            next_new_subscript += 1
+
+    num_new_subscripts = num_idx_a + num_idx_b - num_ext_idx
+    assert next_new_subscript == num_new_subscripts
+
+    new_tensors = [t.conjugate() for t in a.tensors]
+    new_tensors.extend(b.tensors)
+
+    new_subscripts = []
+    f = lambda x: new_subscripts_a[x]
+    new_subscripts.extend([tuple(map(f, tp)) for tp in a.subscripts])
+    f = lambda x: new_subscripts_b[x]
+    new_subscripts.extend([tuple(map(f, tp)) for tp in b.subscripts])
+
+    #print(new_tensors)
+    #print(len(new_tensors))
+    #print(new_subscripts)
+    #print(len(new_subscripts))
+    return TensorNetwork(new_tensors, new_subscripts)
+
+
+
+
+
+
--- test/python/tensor_network_test.py
+++ test/python/tensor_network_test.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 import unittest
 import numpy
-from irbasis_util.tensor_network import Tensor, TensorNetwork
+from irbasis_util.tensor_network import Tensor, TensorNetwork, conj_a_b
 
 
 class TestMethods(unittest.TestCase):
@@ -30,7 +30,7 @@ class TestMethods(unittest.TestCase):
         B = Tensor("B", (N2, N3), True)
         tn = TensorNetwork([A, B], [(0,1),(1,2)])
 
-        numpy.testing.assert_allclose(tn.outer_subscripts, numpy.array([0,2]))
+        numpy.testing.assert_allclose(tn.external_subscripts, numpy.array([0,2]))
 
         tn.find_contraction_path(True)
 
@@ -40,6 +40,35 @@ class TestMethods(unittest.TestCase):
         val = tn.evaluate(vals)
         val_ref = numpy.einsum('ij,jk->ik', vals['A'], vals['B'].conjugate())
         numpy.testing.assert_allclose(val, val_ref)
+
+    def test_conj_a_b(self):
+        """
+
+        """
+        N = 10
+        a1 = Tensor("a1", (N, N, N))
+        a2 = Tensor("a2", (N, N, N))
+        a = TensorNetwork([a1, a2], [(0,2,3), (1,2,3)])
+
+        b1 = Tensor("b1", (N, N))
+        b2 = Tensor("b2", (N, N))
+        b = TensorNetwork([b1, b2], [(0,2), (1,2)])
+
+        c = conj_a_b(a, b)
+
+        a1v = numpy.random.randn(N, N, N) + 1J*numpy.random.randn(N, N, N)
+        a2v = numpy.random.randn(N, N, N) + 1J*numpy.random.randn(N, N, N)
+        av = numpy.einsum('ikl,jkl->ij', a1v, a2v)
+
+        b1v = numpy.random.randn(N, N) + 1J*numpy.random.randn(N, N)
+        b2v = numpy.random.randn(N, N) + 1J*numpy.random.randn(N, N)
+        bv = numpy.einsum('ik,jk->ij', b1v, b2v)
+
+        cv = numpy.einsum('ij,ij', av.conj(), bv)
+
+        c.find_contraction_path()
+        cv2 = c.evaluate({"a1" : a1v, "a2" : a2v, "b1" : b1v, "b2" : b2v})
+        self.assertAlmostEqual(cv, cv2)
 
 
 if __name__ == '__main__':
