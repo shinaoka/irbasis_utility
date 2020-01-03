b01960ab1108f4341ec42fb4b7d185ed4fd23455
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2020-01-03 01:26:16 +0000

    Workaround for errors on GCP

--- python/__init__.py
+++ python/__init__.py
@@ -1,3 +1,5 @@
-from pkg_resources import get_distribution
-
-__version__ = get_distribution('irbasis-utility').version
+try:
+    from pkg_resources import get_distribution
+    __version__ = get_distribution('irbasis-utility').version
+except:
+    __version__ = '0.0.0'
